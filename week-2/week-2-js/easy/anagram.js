904bf4569ccce0cb46db8e049cf0d1658b26f3f0
adithya61 <zonic662@gmail.com>
2026-03-01 04:55:05 +0530

    update

--- week-2/week-2-js/easy/anagram.js
+++ week-2/week-2-js/easy/anagram.js
@@ -5,7 +5,9 @@
 */
 
 function isAnagram(str1, str2) {
-
+  return str1.split("").sort().join("") == str2.split("").sort().join("");
 }
+
+console.log(isAnagram('rasp', 'sap'));
 
 module.exports = isAnagram;
--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -14,7 +14,65 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  let categoryMap = {};
+
+  for (const transaction of transactions) {
+    const { category, price } = transaction;
+
+    if (!categoryMap[category]) {
+      categoryMap[category] = 0;
+    }
+
+    categoryMap[category] += price;
+  }
+
+  let result = [];
+
+  for (const key in categoryMap) {
+    result.push({ category: key, totalSpent: categoryMap[key] });
+  }
+
+  return result;
 }
+
+let transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 12,
+    category: "Food",
+    itemName: "Pizza",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 13.5,
+    category: "Shopping",
+    itemName: "Shirt",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 16.75,
+    category: "Shopping",
+    itemName: "Hoodie",
+  },
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 18,
+    category: "Furniture",
+    itemName: "Table",
+  },
+];
+
+console.log(calculateTotalSpentByCategory(transactions));
 
 module.exports = calculateTotalSpentByCategory;
