03df964e4482759358667b331fb3b4f4def692e8
adithya61 <zonic662@gmail.com>
2026-03-02 04:38:22 +0530

    solution

--- week-2/week-2-js/easy/findLargestElement.js
+++ week-2/week-2-js/easy/findLargestElement.js
@@ -6,7 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let maxi = -1;
+    for(let num of numbers){
+        if(num > maxi) maxi = num;
+    }
+    return maxi;
 }
+
+console.log(findLargestElement([1,2,3,4,5,6]))
 
 module.exports = findLargestElement;
