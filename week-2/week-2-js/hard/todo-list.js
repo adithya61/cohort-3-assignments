0efbb93f58b03d067aeaf03526be3769ff31b3df
adithya61 <zonic662@gmail.com>
2026-03-12 23:44:45 +0530

    todo-list solution

--- week-2/week-2-js/hard/todo-list.js
+++ week-2/week-2-js/hard/todo-list.js
@@ -7,11 +7,51 @@
     - get(indexOfTodo): returns todo at given index
     - clear: deletes all todos
 
+
   Once you've implemented the logic, test your code by running
 */
 
 class Todo {
+  constructor() {
+    this.todos = [];
+  }
 
+  add(todo) {
+    this.todos.push(todo);
+  }
+
+  remove(index) {
+    if (index < 0 || index > this.todos.length - 1) return null;
+    this.todos.splice(index, 1);
+  }
+
+  update(index, updatedTodo) {
+    if (index > this.todos.length - 1) return null;
+    this.todos[index] = updatedTodo;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  get(index) {
+    if (index > this.todos.length - 1) return null;
+    return this.todos[index];
+  }
+
+  clear() {
+    this.todos = [];
+  }
 }
+
+let obj = new Todo();
+obj.add("one");
+obj.add("two");
+obj.add("three");
+
+obj.update(1, "updated two");
+
+console.log(obj.getAll());
+console.log(obj.get(1));
 
 module.exports = Todo;
